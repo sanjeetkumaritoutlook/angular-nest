2a96cad7020e60d41b98da6bbb472dfae120cc36
sanjeetkumaritoutlook-user <sanjeetkumarit@outlook.com>
2025-09-05 20:14:01 +0530

    removed mongodb for now for Render deploymement

--- nest-proxy/src/app.module.ts
+++ nest-proxy/src/app.module.ts
@@ -2,14 +2,14 @@ import { Module } from '@nestjs/common';
 // import { AppController } from './app.controller';
 // import { AppService } from './app.service';
 //import { UsersController } from './users/users.controller';
-import { MongooseModule } from '@nestjs/mongoose';
-import { UsersModule } from './users/users.module';
+//import { MongooseModule } from '@nestjs/mongoose';
+//import { UsersModule } from './users/users.module';
 import { UploadModule } from './upload/upload.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot( process.env.MONGO_URI || 'mongodb://localhost:27017/mydb'),
-    UsersModule,
+   // MongooseModule.forRoot( process.env.MONGO_URI || 'mongodb://localhost:27017/mydb'),
+   // UsersModule,
     UploadModule
   ],
   // controllers: [AppController, UsersController],
